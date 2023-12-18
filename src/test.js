8225299ed4891bea329fcd8bce26963279b13481
AnNinh2605 <anninh1511@gmail.com>
2023-12-18 17:16:50 +0700

    #26 - #31

--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path');
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('./routes/web')
-
+const connection = require('./config/database');
 // import express form 'express';
 const app = express()
 const port = process.env.PORT || 8101;
@@ -14,6 +14,14 @@ configViewEngine(app);
 
 //route
 app.use('/', webRoutes)
+
+// simple query
+connection.query(
+  'SELECT * FROM Users u',
+  function(err, results, fields) {
+    console.log("===", results); // results contains rows returned by server
+  }
+);
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`)
